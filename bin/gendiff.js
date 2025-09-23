82929ca391513f4f104aa62f37a85d47a3e0b96a
Ivan <gimo135@yandex.ru>
2025-09-23 15:20:16 +0300

    Add help flag using commander.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 import { program } from 'commander'
 
-program.
-  description('')
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+
+program.parse()
