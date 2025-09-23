22ebf59ec76ea2583fdcdc257d0425f209a8fa31
Ivan <gimo135@yandex.ru>
2025-09-23 15:48:02 +0300

    Add new flag and arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,5 +5,8 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
+  .option('-f, --format', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
 
 program.parse()
