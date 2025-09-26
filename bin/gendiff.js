2b540f599960f77bce953ef75c31b522d5e27531
Ivan <gimo135@yandex.ru>
2025-09-26 20:35:27 +0300

    Add getDiff.js and implement it in gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { program } from 'commander'
 import parser from '../src/parser.js'
+import getDiff from '../src/getDiff.js'
 
 program
   .name('gendiff')
@@ -12,7 +13,8 @@ program
   .action((filepath1, filepath2) => {
     const file1 = parser(filepath1)
     const file2 = parser(filepath2)
-    console.log(file1, file2)
+    const diff = getDiff(file1, file2)
+    console.log(diff)
   })
 
 program.parse()
---
+++ src/getDiff.js
@@ -0,0 +1,25 @@
+import sortBy from 'lodash/sortBy.js'
+
+const getDiff = (file1, file2) => {
+  const set = new Set([...Object.keys(file1), ...Object.keys(file2)])
+  const unique = sortBy(Array.from(set))
+  let resultString = ''
+  for (const key of unique) {
+    if (file1[key] === file2[key]) {
+      resultString += `${key}: ${file1[key]}\n`
+    }
+    else if ((key in file1) && !(key in file2)) {
+      resultString += `- ${key}: ${file1[key]}\n`
+    }
+    else if (!(key in file1) && (key in file2)) {
+      resultString += `+ ${key}: ${file2[key]}\n`
+    }
+    else {
+      resultString += `- ${key}: ${file1[key]}\n`
+      resultString += `+ ${key}: ${file2[key]}\n`
+    }
+  }
+  return `{\n${resultString}}`
+}
+
+export default getDiff
