22bf96416a0dd5db7d2b94fd5a0dab683107cf73
Ivan <gimo135@yandex.ru>
2025-10-09 17:04:18 +0300

    Update __tests__ for yaml comparing and update parser.js

--- __tests__/getDiff.test.js
+++ __tests__/getDiff.test.js
@@ -4,14 +4,16 @@ import fs from 'fs'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
+const formats = ['json', 'yml']
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const getFixturePath = fileName => path.resolve(__dirname, '..', '__fixtures__', fileName)
 const fixturePath = getFixturePath('getDiffResult.txt')
-const file1Path = getFixturePath('file1.json')
-const file2Path = getFixturePath('file2.json')
 const fixture = fs.readFileSync(fixturePath, 'utf-8')
 
-test('getDiff', () => {
+test.each(formats)('getDiff', (format) => {
+  const file1Path = getFixturePath(`file1.${format}`)
+  const file2Path = getFixturePath(`file2.${format}`)
   expect(genDiff(file1Path, file2Path)).toBe(fixture)
 })
--- src/parser.js
+++ src/parser.js
@@ -1,10 +1,16 @@
 import fs from 'fs'
 import path from 'path'
 import process from 'process'
+import yaml from 'js-yaml'
 
 const parser = (filepath) => {
   const absolutePath = path.resolve(process.cwd(), filepath)
+  const fileType = absolutePath.split('.').at(-1)
   const content = fs.readFileSync(absolutePath)
+  if (fileType === 'yml' || fileType === 'yaml') {
+    const parsed = yaml.load(content)
+    return parsed
+  }
   const parsed = JSON.parse(content)
   return parsed
 }
