677b38def84c9f89c20835bce9ba8510bd7ca51b
Ivan <gimo135@yandex.ru>
2025-11-06 16:44:41 +0300

    Update plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -4,7 +4,7 @@ const stringify = (value) => {
   if (isPlainObject(value)) {
     return '[complex value]'
   }
-  if (typeof (value) === 'boolean' || value === null) {
+  if (typeof (value) === 'boolean' || value === null || typeof (value) === 'number') {
     return `${value}`
   }
   return `'${value}'`
