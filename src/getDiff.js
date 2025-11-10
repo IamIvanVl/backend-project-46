3bdd64812245f05ab79e2a46008e923f8ee89a7f
Ivan <gimo135@yandex.ru>
2025-11-10 16:13:01 +0300

    Refactor getDiff.js

--- src/getDiff.js
+++ src/getDiff.js
@@ -1,29 +1,24 @@
-import sortBy from 'lodash/sortBy.js'
 import isPlainObject from 'lodash/isPlainObject.js'
 
 const getDiff = (file1, file2) => {
   const set = new Set([...Object.keys(file1), ...Object.keys(file2)])
-  const unique = sortBy(Array.from(set))
+  const unique = Array.from(set).toSorted()
 
   const ast = unique.map((key) => {
-    if ((key in file1) && !(key in file2)) {
-      key = { key, value: file1[key], status: 'deleted' }
-      return key
+    if (!Object.hasOwn(file2, key)) {
+      return { key, value: file1[key], status: 'deleted' }
     }
-    else if (!(key in file1) && (key in file2)) {
-      key = { key, value: file2[key], status: 'added' }
-      return key
+    else if (!Object.hasOwn(file1, key)) {
+      return { key, value: file2[key], status: 'added' }
     }
     else if (isPlainObject(file1[key]) && isPlainObject(file2[key])) {
       return { key, value: getDiff(file1[key], file2[key]), status: 'nested' }
     }
     else if (file1[key] === file2[key]) {
-      key = { key, value: file1[key], status: 'unchanged' }
-      return key
+      return { key, value: file1[key], status: 'unchanged' }
     }
     else if (file1[key] !== file2[key]) {
-      key = { key, value: file1[key], newValue: file2[key], status: 'changed' }
-      return key
+      return { key, value: file1[key], newValue: file2[key], status: 'changed' }
     }
   })
   return ast
