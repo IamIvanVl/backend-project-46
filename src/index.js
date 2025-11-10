83c37779c11011e105b3c6d2de46fda25fd610f9
Ivan <gimo135@yandex.ru>
2025-11-10 16:30:01 +0300

    Remove file related logic from parser

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import parser from '../src/parser.js'
 import getDiff from '../src/getDiff.js'
 import getFormat from './formatters/index.js'
+import fs from 'fs'
+import path from 'path'
+import process from 'process'
+
+const readFile = (filepath) => {
+  const absolutePath = path.resolve(process.cwd(), filepath)
+  return fs.readFileSync(absolutePath)
+}
+const getExtension = filepath => path.extname(filepath).slice(1)
 
 export const genDiff = (filepath1, filepath2, format = 'stylish') => {
-  const file1 = parser(filepath1)
-  const file2 = parser(filepath2)
+  const content1 = readFile(filepath1)
+  const content2 = readFile(filepath2)
+  const file1 = parser(content1, getExtension(filepath1))
+  const file2 = parser(content2, getExtension(filepath2))
   const diff = getDiff(file1, file2)
   const formatter = getFormat(format)
   return formatter(diff)
--- src/parser.js
+++ src/parser.js
@@ -1,12 +1,6 @@
-import fs from 'fs'
-import path from 'path'
-import process from 'process'
 import yaml from 'js-yaml'
 
-const parser = (filepath) => {
-  const absolutePath = path.resolve(process.cwd(), filepath)
-  const fileType = absolutePath.split('.').at(-1)
-  const content = fs.readFileSync(absolutePath)
+const parser = (content, fileType) => {
   if (fileType === 'yml' || fileType === 'yaml') {
     const parsed = yaml.load(content)
     return parsed
