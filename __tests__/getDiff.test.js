edbb7d8abee58d8d060d0bf483542e33d9dfc988
Ivan <gimo135@yandex.ru>
2025-11-10 16:16:18 +0300

    Clean tests

--- __tests__/getDiff.test.js
+++ __tests__/getDiff.test.js
@@ -9,25 +9,15 @@ const formats = ['json', 'yml']
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const getFixturePath = fileName => path.resolve(__dirname, '..', '__fixtures__', fileName)
-const fixturePath = getFixturePath('getDiffResult.txt')
-const fixture = fs.readFileSync(fixturePath, 'utf-8')
+const expectedStylish = fs.readFileSync(getFixturePath('stylishResult.txt'), 'utf-8')
+const expectedPlain = fs.readFileSync(getFixturePath('plainResult.txt'), 'utf-8')
+const expectedJson = fs.readFileSync(getFixturePath('jsonResult.json'), 'utf-8')
 
-test.each(formats)('getDiff', (format) => {
+test.each(formats)('getDiff %s', (format) => {
   const file1Path = getFixturePath(`file1.${format}`)
   const file2Path = getFixturePath(`file2.${format}`)
-  expect(genDiff(file1Path, file2Path, 'stylish')).toBe(fixture)
-})
 
-test.each(formats)('getDiff plain', (format) => {
-  const file1Path = getFixturePath(`file1.${format}`)
-  const file2Path = getFixturePath(`file2.${format}`)
-  const expected = fs.readFileSync(getFixturePath('plainResult.txt'), 'utf-8')
-  expect(genDiff(file1Path, file2Path, 'plain')).toBe(expected)
-})
-
-test.each(formats)('getDiff plain', (format) => {
-  const file1Path = getFixturePath(`file1.${format}`)
-  const file2Path = getFixturePath(`file2.${format}`)
-  const expected = fs.readFileSync(getFixturePath('jsonResult.json'), 'utf-8')
-  expect(genDiff(file1Path, file2Path, 'json')).toBe(expected)
+  expect(genDiff(file1Path, file2Path, 'stylish')).toBe(expectedStylish)
+  expect(genDiff(file1Path, file2Path, 'plain')).toBe(expectedPlain)
+  expect(genDiff(file1Path, file2Path, 'json')).toBe(expectedJson)
 })
